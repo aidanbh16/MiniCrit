dafac052529d5bbc15a08133222ff932a66d7003
Aidan Holton <aidanbh16@gmail.com>
2025-08-17 16:50:50 -0400

    color scheme list

--- app/(client-side)/auth/login/login.tsx
+++ app/(client-side)/auth/login/login.tsx
@@ -1,10 +1,10 @@
 export default function login(){
     return(
-        <form action="" className="w-2/3 h-2/3 border-1 border-black bg-[rgb(18, 20, 18)] text-black">
-            <h1>LOGIN</h1>
-            <label>Email</label>
+        <form action="" className="w-2/3 h-2/3 border-1 border-black bg-[rgb(18, 20, 18)] text-black dark:bg-[rgba(18,20,38,1)]">
+            <h1 className="dark:text-white">LOGIN</h1>
+            <label className="dark:text-white">Email</label>
             <input type="email" className="border-black bg-amber-400"></input>
-            <label>Password</label>
+            <label className="dark:text-white">Password</label>
             <input type="password" className="border-black bg-amber-400"></input>
         </form>
     )
--- app/(client-side)/components/layout/nav.tsx
+++ app/(client-side)/components/layout/nav.tsx
@@ -21,9 +21,9 @@ export default function Nav() {
 
   return (
     <>
-      <header className="flex items-center h-15 w-full min-w-98 fixed z-51 bg-[rgba(234,235,237,1)] dark:bg-[rgba(18,20,18,1)] max-md:h-10">
+      <header className="flex items-center h-15 w-full min-w-98 fixed z-51 bg-[rgba(234,235,237,1)] dark:bg-[rgba(46,23,96,1)] max-md:h-10">
         <div className="w-1/3 h-full flex justify-center max-md:w-1/2 max-md:min-w-1/2 max-md:justify-start">
-          <div className="h-23 flex items-center bg-neutral-800 dark:bg-[rgba(50,150,255,1)] max-md:hidden">
+          <div className="h-23 flex items-center bg-neutral-800 dark:bg-[rgba(23,23,56,1)] max-md:hidden">
             <Link href="/"><Image src="/logo/logo2.png" alt="Logo" height={15} width={200} /></Link>
           </div>
           <Link href="/"><Image src="/logo/logo1.png" alt="Logo" className="min-md:hidden dark:invert" height={10} width={150} /></Link>
@@ -31,21 +31,21 @@ export default function Nav() {
         <div className="w-1/3 h-full max-md:hidden"></div>
         <nav className="w-1/3 h-full max-md:w-1/2 max-md:min-w-1/2">
           <div className="h-full w-full flex justify-center items-center max-md:hidden">
-            <div className="hover:bg-neutral-300 dark:hover:bg-neutral-950 flex justify-center items-center w-full h-full">
-                <Link href="/" className="text-black dark:text-[rgba(50,150,255,1)] dark:hover:text-white">Home</Link>
-            </div>
-            <div className="hover:bg-neutral-300 dark:hover:bg-neutral-950 flex justify-center items-center w-full h-full">
-                <Link href="/auth/login" className="text-black dark:text-[rgba(50,150,255,1)] dark:hover:text-white">Login</Link>
-            </div>
-            <div className="hover:bg-neutral-300 dark:hover:bg-neutral-950 flex justify-center items-center w-full h-full">
-                <Link href="/profile" className="text-black dark:text-[rgba(50,150,255,1)] dark:hover:text-white">Profile</Link>
-            </div>
-            <div className="hover:bg-neutral-300 dark:hover:bg-neutral-950 flex justify-center items-center w-full h-full">
-                <Link href="/about" className="text-black dark:text-[rgba(50,150,255,1)] dark:hover:text-white">About</Link>
-            </div>
-            <div className="hover:bg-neutral-300 dark:hover:bg-neutral-950 flex justify-center items-center w-full h-full">
-                <Link href="/updates" className="text-black dark:text-[rgba(50,150,255,1)] dark:hover:text-white">Updates</Link>
-            </div>
+            <Link href="/" className="hover:bg-neutral-300 dark:hover:bg-neutral-950 flex justify-center items-center w-full h-full dark:hover:*:text-[rgba(125,131,255,1)]">
+                <div className="text-black dark:text-white">Home</div>
+            </Link>
+            <Link href="/auth/login" className="hover:bg-neutral-300 dark:hover:bg-neutral-950 flex justify-center items-center w-full h-full dark:hover:*:text-[rgba(125,131,255,1)]">
+                <div className="text-black dark:text-white">Login</div>
+            </Link>
+            <Link href="/profile" className="hover:bg-neutral-300 dark:hover:bg-neutral-950 flex justify-center items-center w-full h-full dark:hover:*:text-[rgba(125,131,255,1)]">
+                <div className="text-black dark:text-white">Profile</div>
+            </Link>
+            <Link href="/about" className="hover:bg-neutral-300 dark:hover:bg-neutral-950 flex justify-center items-center w-full h-full dark:hover:*:text-[rgba(125,131,255,1)]">
+                <div className="text-black dark:text-white">About</div>
+            </Link>
+            <Link href="/updates" className="hover:bg-neutral-300 dark:hover:bg-neutral-950 flex justify-center items-center w-full h-full dark:hover:*:text-[rgba(125,131,255,1)]">
+                <div className="text-black dark:text-white">Updates</div>
+            </Link>
             <div className="flex justify-center items-center w-full h-full">
                 <DarkModeToggle/>
             </div>
