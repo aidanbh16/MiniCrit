2485e76d5161a5a27218eeff0e847a7e0f017863
Aidan <aidanbh16@gmail.com>
2025-08-25 18:36:55 -0400

    changes, started db connection file, wrote base table creation (insert_users to be deleted)

---
+++ sql/init_users.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS users (
+    id  UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    username VARCHAR(32) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    pass TEXT NOT NULL,
+    is_admin BOOLEAN NOT NULL DEFAULT false,
+    descript TEXT,
+    pfp_key TEXT,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    is_active BOOLEAN NOT NULL DEFAULT true
+)
---
+++ sql/insert_users.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (username, email, password_hash, descript, pfp_key) 
+VALUES ('username', 'test@gmail.com', 'password', 'hello world', 'key')
