0741bcf4b81a59e61bf4c15703df6b425eb8608b
Aidan <aidanbh16@gmail.com>
2025-09-05 16:57:33 -0400

    fix amplify connecting to db (hopefully)

--- src/lib/compare.ts
+++ src/lib/compare.ts
@@ -1,6 +1,7 @@
 import "server-only"
 import bcrypt from "bcrypt"
 import pool from "@/lib/db"
+export const runtime = 'nodejs'
 
 export async function compare(user: string, pass: string): Promise<boolean> {
     const { rows } = await pool.query('SELECT password_hash FROM users WHERE username=$1', [user]);
