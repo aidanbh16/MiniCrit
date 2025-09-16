0344688ad606727a581fdc0848d101d46901fdad
Aidan <aidanbh16@gmail.com>
2025-09-16 10:55:50 -0400

    database pool connection fixed for real this time

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,13 @@
 import 'server-only'
 import { Pool } from 'pg'
 
-const SSL_CONFIG = process.env.SSL_CONFIG ? JSON.parse(process.env.SSL_CONFIG) : false
-
 const pool = new Pool({
-    connectionString: process.env.DB_URL!,
-    ssl: SSL_CONFIG!,
+    user: process.env.DB_USER!,
+    password: process.env.DB_PASS!,
+    host: process.env.DB_HOST!,
+    port: Number(process.env.DB_PORT)!,
+    database: process.env.DB_NAME!,
+    ssl: { rejectUnauthorized: false },
 })
 
 export default pool;
-
-// retrigger comment
