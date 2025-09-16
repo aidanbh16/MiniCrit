37f88f46fa9c72096de0e380ff337a0e97d4a074
Aidan <aidanbh16@gmail.com>
2025-09-16 09:55:15 -0400

    Switched database pool connection to pure connectionString

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,11 +4,6 @@ import { Pool } from 'pg'
 const SSL_CONFIG = process.env.SSL_CONFIG ? JSON.parse(process.env.SSL_CONFIG) : false
 
 const pool = new Pool({
-    user: process.env.DB_USER!,
-    password: process.env.DB_PASS!,
-    host: process.env.DB_HOST!,
-    port: Number(process.env.DB_PORT)!,
-    database: process.env.DB_NAME!,
     connectionString: process.env.DB_URL!,
     ssl: SSL_CONFIG!,
 })
